33bf1cd8b9cb46a063a5d81e3e762af81aae878c
DDromaDD <133244366+DDromaDD@users.noreply.github.com>
2023-05-11 20:14:24 +0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+//вывод текста в консоль
+
+    void Start()
+    {
+        //вывод текста в консоль
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
